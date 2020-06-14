23ad5a84d3bc07a5726eaebfbe1b77c099dcbbed
baixiaoji <1041874421@qq.com>
2020-06-14 15:29:52 +0800

    using next-images plugin to replace before

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-module.exports = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.png|jpg|jpeg|gif|svg$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[contenthash].[ext]',
-            outputPath: 'static',// 硬盘路径
-            publicPath: '_next/static',  // 网站路径
-          },
-        }
-      ],
-    })
-
-    return config;
+// next.config.js
+const withImages = require('next-images')
+module.exports = withImages({
+  webpack(config, options) {
+    return config
   }
-}
+})
