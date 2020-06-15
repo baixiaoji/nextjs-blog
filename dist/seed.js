7597505dfa1bf53d6c93523877ad78a7f1c70c00
baixiaoji <1041874421@qq.com>
2020-06-16 07:40:56 +0800

    create post entity and having the seed method

--- dist/seed.js
+++ dist/seed.js
@@ -10,17 +10,39 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _Post = require("./entity/Post");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var posts;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log(connection);
-            _context.next = 3;
+            _context.next = 2;
+            return connection.manager.find(_Post.Post);
+
+          case 2:
+            posts = _context.sent;
+
+            if (!(posts.length === 0)) {
+              _context.next = 6;
+              break;
+            }
+
+            _context.next = 6;
+            return connection.manager.save([2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(function (n) {
+              return new _Post.Post({
+                title: "Post ".concat(n),
+                content: "\u6211\u7684\u7B2C".concat(n, "\u7BC7\u535A\u5BA2")
+              });
+            }));
+
+          case 6:
+            _context.next = 8;
             return connection.close();
 
-          case 3:
+          case 8:
           case "end":
             return _context.stop();
         }
